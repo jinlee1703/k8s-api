46ed736c30b39b63780b0d5fcb35ab885c1798fc
Jinwoo Lee <binaryWooo@gmail.com>
2024-10-19 09:14:24 +0900

    feat: 데이터베이스 연동 #2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use std::env;
+
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+
+mod db;
 
 #[get("/api/health")]
 async fn health_check() -> impl Responder {
@@ -10,10 +15,16 @@ async fn health_check() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    env_logger::init();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = db::init_db(&database_url).await.expect("Failed to initialize database");
+
     println!("서버 시작 중...");
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+        .app_data(web::Data::new(pool.clone()))
         .service(health_check)
     })
     .bind("127.0.0.1:8080")?        
